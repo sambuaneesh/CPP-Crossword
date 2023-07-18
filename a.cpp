5987d647f55e492d52e3caaf8da78f98c194ca9d
Aneesh Sambu <sambuaneesh@gmail.com>
2023-07-18 14:28:16 +0530

    Loading and Stats of Library

--- a.cpp
+++ a.cpp
@@ -4,6 +4,53 @@
 #include <assert.h>
 #include <fstream>
 
+// Things inside a class are private by default
+class Library {
+public:
+  void
+    ComputeStats() {
+    assert(counts.empty());
+    counts.resize(18);
+    for (std::string str : words) {
+      int len = str.size();
+      if (len)
+        ++counts[len];
+    }
+  }
+  void
+    PrintStats() const {
+    std::cout << "Stats of the Library Data are: "
+      << std::endl;
+    for (int i = 1;i < counts.size();i++) {
+      std::cout << "[ " << i << " ]\t"
+        << counts[i] << std::endl;
+    }
+  }
+  std::string
+    GetWord(int i) const {
+    assert(i >= 0 && i < words.size());
+    return words[i];
+  }
+  void
+    ReadFromFile(std::string filename) {
+    std::fstream file;
+    file.open(filename);
+    while (!file.eof()) {
+      std::string line;
+      getline(file, line);
+      if (!line.empty())
+        words.push_back(line);
+    }
+    std::cout << "Successfully read " << words.size()
+      << " words from " << filename << std::endl;
+  }
+
+private:
+  std::vector<std::string> words;
+  std::vector<int> counts;
+};
+
+// Things inside a struct are public by default
 struct Grid {
   std::vector<std::string> lines;
   std::string name;
@@ -49,5 +96,9 @@ main() {
   grid.Load("grid");
   grid.Check();
   grid.Print();
+  Library lib;
+  lib.ReadFromFile("wordlist.txt");
+  lib.ComputeStats();
+  lib.PrintStats();
 }
 
